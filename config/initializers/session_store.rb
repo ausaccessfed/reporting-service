afaa1f350e7163e7e9584405664d88381ad53133
Shaun Mangelsdorf <s.mangelsdorf@gmail.com>
2015-08-07 14:25:13 +1000

    Use Redis for session storage

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,8 @@
-Rails.application.config.session_store :cookie_store, key: '_reporting-service_session'
+redis_namespace = "reporting-service:#{Rails.env}:session"
+session_store_opts = {
+  redis_server: "redis://127.0.0.1:6379/0/#{redis_namespace}",
+  expire_in: 3600,
+  key: '_reporting-service-session'
+}
+
+Rails.application.config.session_store(:redis_store, session_store_opts)
