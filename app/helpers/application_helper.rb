68a3fdd481ef6a857bf79ee3d59f47e30fb8f144
Shaun Mangelsdorf <s.mangelsdorf@gmail.com>
2015-08-17 13:23:59 +1000

    Add AAF style from aaf-lipstick

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  include Lipstick::Helpers::LayoutHelper
+  include Lipstick::Helpers::NavHelper
+  include Lipstick::Helpers::FormHelper
+
+  VERSION = '0.0.1'
+
+  def permitted?(*)
+    true # TODO
+  end
+
+  def environment_string
+    'TODO' # TODO
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,4 @@
 Rails.application.routes.draw do
+  get '/dashboard' => 'dashboard#index', as: 'dashboard'
+  root to: 'welcome#index'
 end
