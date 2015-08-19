f13158308237cb2985919685fb74063b55e58605
Shaun Mangelsdorf <s.mangelsdorf@gmail.com>
2015-08-19 14:16:27 +1000

    Add Gumboot-compliant ApplicationController

---
+++ spec/controllers/application_controller_spec.rb
@@ -0,0 +1,6 @@
+require 'rails_helper'
+require 'gumboot/shared_examples/application_controller'
+
+RSpec.describe ApplicationController, type: :controller do
+  include_examples 'Application controller'
+end
