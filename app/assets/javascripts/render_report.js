956aaeaacb7ab5bf5bd33ff3538f14d84ec2ae85
Shaun Mangelsdorf <s.mangelsdorf@gmail.com>
2015-09-04 07:40:53 +1000

    Reduce UI latency by doing less frequent lookups
    The frequency of mouseover events was causing the updates to lag behind
    the mouse pointer by a noticeable amount. The most expensive part of the
    mouseover event is looking up the data point.
    
    This change throttles the updates with a 50ms window. This is enough to
    still feel instant but significantly reduce the overhead.

--- app/assets/javascripts/render_report.js
+++ app/assets/javascripts/render_report.js
@@ -2,6 +2,27 @@
 
 jQuery(function($) {
   var renderReport = function(report) {
+    function throttle(func, timeout) {
+      var next = null, timer = null;
+      return function() {
+        var context = this, args = arguments;
+
+        var invoke = function() {
+          if (next) next();
+          timer = null;
+          next = null;
+        };
+
+        if (!timer) {
+          timer = setTimeout(invoke, timeout);
+          func.apply(context, args);
+          return;
+        }
+
+        next = function() { func.apply(context, args); };
+      };
+    };
+
     var margin = { top: 20, right: 30, bottom: 30, left: 50 };
     var legend = { width: 160, margin: 20 };
 
@@ -10,6 +31,7 @@ jQuery(function($) {
 
     var timeOnly = d3.time.format("%H:%M");
     var timeFormat = d3.time.format('%Y-%m-%d %H:%M:%S UTC');
+
     var range = {
       start: timeFormat.parse(report.range.start),
       end: timeFormat.parse(report.range.end)
@@ -123,6 +145,27 @@ jQuery(function($) {
 
         var bisectDate = d3.bisector(function(d) { return d[0]; }).left;
 
+        var update = throttle(function(mouse) {
+          var date = 0;
+          var x0 = (scale.x.invert(mouse[0]) - range.start) / 1000;
+
+          report.series.forEach(function(k) {
+            var data = report.data[k];
+            var i = bisectDate(data, x0, 1),
+            d0 = data[i - 1],
+            d1 = data[i],
+            d = x0 - d0[0] > d1[0] - x0 ? d1 : d0;
+
+            date = Math.abs(x0 - d[0]) > Math.abs(x0 - date) ? date : d[0];
+
+            svg.select('.legend .' + k + '-text')
+              .text(d3.round(d[1], 1) + ' ms');
+          });
+
+          svg.select('.x-text').text(
+              timeOnly(d3.time.second.offset(range.start, date)));
+        }, 50);
+
         g.append("rect")
           .attr("class", "hover-box")
           .attr("width", width)
@@ -139,24 +182,7 @@ jQuery(function($) {
           })
           .on("mousemove", function() {
             var mouse = d3.mouse(this);
-            var x0 = (scale.x.invert(mouse[0]) - range.start) / 1000;
-            var date = 0;
-
-            report.series.forEach(function(k) {
-              var data = report.data[k];
-              var i = bisectDate(data, x0, 1),
-                d0 = data[i - 1],
-                d1 = data[i],
-                d = x0 - d0[0] > d1[0] - x0 ? d1 : d0;
-
-              date = Math.abs(x0 - d[0]) > Math.abs(x0 - date) ? date : d[0];
-
-              svg.select('.legend .' + k + '-text')
-                .text(d3.round(d[1], 1) + ' ms');
-            });
-
-            svg.select('.x-text').text(
-              timeOnly(d3.time.second.offset(range.start, date)));
+            update(mouse);
 
             var pos = mouse[0];
             hoverbar.attr("x1", pos).attr("x2", pos);
