f99929c970a5715c16254cc630d462aa8b4a70f2
Shaun Mangelsdorf <s.mangelsdorf@gmail.com>
2015-09-07 14:26:38 +1000

    Fix Rubocop violations in temporary code
    The code will stay intact while this branch is reviewed, and removed in
    future. For now, it makes life easier if it's Rubocop-compliant.

--- app/reports/random_time_series_report.rb
+++ app/reports/random_time_series_report.rb
@@ -14,18 +14,29 @@ class RandomTimeSeriesReport < TimeSeriesReport
   end
 
   def data
-    rng = Distribution::Normal.rng
-    range = (0..(@finish.to_i - @start.to_i)).step(1800)
-
     range.each_with_object(total: [], database: [], render: []) do |n, map|
-      r1 = rng.call.abs * 5
-      r2 = rng.call * 3
-      r2 = -2.0 if r2 < -2.0
-      r3 = rng.call.abs * 5
-
+      r1, r2, r3 = randoms
       map[:database] << [n, 30 + r2]
       map[:render] << [n, 10 + r1]
       map[:total] << [n, 40 + r1 + r2 + r3]
     end
+  end
+
+  private
+
+  def rng
+    @rng ||= Distribution::Normal.rng
+  end
+
+  def range
+    (0..(@finish.to_i - @start.to_i)).step(1800)
+  end
+
+  def randoms
+    r1 = rng.call.abs * 5
+    r2 = rng.call * 3
+    r2 = -2.0 if r2 < -2.0
+    r3 = rng.call.abs * 5
+    [r1, r2, r3]
   end
 end
