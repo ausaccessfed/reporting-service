2ddb0cb9b9b05045b56917fc30df9dd2727b5c92
toomaj <toomaj_b@yahoo.com>
2015-10-28 12:45:21 +1000

    Change :type column in rapid_connect_services to :service_type

--- spec/factories/rapid_connect_service.rb
+++ spec/factories/rapid_connect_service.rb
@@ -2,6 +2,6 @@ FactoryGirl.define do
   factory :rapid_connect_service do
     identifier { SecureRandom.urlsafe_base64 }
     name { Faker::Lorem.sentence }
-    type { 'research' }
+    service_type { 'research' }
   end
 end
