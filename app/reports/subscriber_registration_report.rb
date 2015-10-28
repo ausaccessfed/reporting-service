fca50bff02d1288ae5d555d716d7636aab7f4b69
toomaj <toomaj_b@yahoo.com>
2015-10-29 09:06:18 +1000

    Improve subscribers_list and add standard_title

--- app/reports/subscriber_registration_report.rb
+++ app/reports/subscriber_registration_report.rb
@@ -1,11 +1,12 @@
 class SubscriberRegistrationReport < TabularReport
   report_type 'subscriber-registrations'
   header ['Name', 'Registration Date']
-  footer ['Name', 'Registration Date']
+  footer
 
-  def initialize(title, indentifier)
+  def initialize(identifier)
+    @identifier = identifier
+    title = standard_title('Registered')
     super(title)
-    @identifier = indentifier
   end
 
   def rows
@@ -15,25 +16,23 @@ class SubscriberRegistrationReport < TabularReport
     end
   end
 
-  # rubocop:disable Metrics/MethodLength
   def subscribers_list
-    case @identifier
-    when 'organizations'
-      Organization.all
-    when 'identity_providers'
-      IdentityProvider.all
-    when 'service_providers'
-      ServiceProvider.all
-    when 'rapid_connect_services'
-      RapidConnectService.all
-    when 'services'
-      ServiceProvider.all + RapidConnectService.all
-    end
+    services = ServiceProvider.all + RapidConnectService.all
+    registered_objects = { 'organizations' => Organization.all,
+                           'identity_providers' => IdentityProvider.all,
+                           'service_providers' => ServiceProvider.all,
+                           'rapid_connect_services' => RapidConnectService.all,
+                           'services' =>  services }
+    registered_objects[@identifier]
   end
 
   def select_activated_subscribers
     subscribers_list.select do |i|
       i.activations.select(&:deactivated_at) == []
     end
+  end
+
+  def standard_title(prefix)
+    prefix.strip.downcase.titleize + ' ' + @identifier.strip.downcase.titleize
   end
 end
--- spec/reports/subscriber_registration_report_spec.rb
+++ spec/reports/subscriber_registration_report_spec.rb
@@ -1,9 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe SubscriberRegistrationReport do
-  let(:title) { Faker::Lorem.sentence }
   let(:header) { [['Name', 'Registration Date']] }
-  let(:footer) { [['Name', 'Registration Date']] }
   let(:type) { 'subscriber-registrations' }
 
   let(:activated_organization) { create(:organization) }
@@ -12,23 +10,23 @@ RSpec.describe SubscriberRegistrationReport do
   let(:activated_rapid_connect) { create(:rapid_connect_service) }
 
   let(:org_report) do
-    SubscriberRegistrationReport.new(title, 'organizations')
+    SubscriberRegistrationReport.new('organizations')
   end
 
   let(:idp_report) do
-    SubscriberRegistrationReport.new(title, 'identity_providers')
+    SubscriberRegistrationReport.new('identity_providers')
   end
 
   let(:sp_report) do
-    SubscriberRegistrationReport.new(title, 'service_providers')
+    SubscriberRegistrationReport.new('service_providers')
   end
 
   let(:rapid_c_report) do
-    SubscriberRegistrationReport.new(title, 'rapid_connect_services')
+    SubscriberRegistrationReport.new('rapid_connect_services')
   end
 
   let(:service_report) do
-    SubscriberRegistrationReport.new(title, 'services')
+    SubscriberRegistrationReport.new('services')
   end
 
   context '#rows' do
@@ -82,29 +80,38 @@ RSpec.describe SubscriberRegistrationReport do
   end
 
   context '#generate' do
-    it 'must have title, header and type' do
-      expect(org_report.generate).to include(title: title, header: header,
-                                             footer: footer, type: type)
+    def standard_title(identifier)
+      'Registered' + ' ' + identifier.strip.downcase.titleize
     end
 
     it 'must have title, header and type' do
-      expect(idp_report.generate).to include(title: title, header: header,
-                                             footer: footer, type: type)
+      title = standard_title 'organizations'
+      expect(org_report.generate).to include(title: title,
+                                             header: header, type: type)
     end
 
     it 'must have title, header and type' do
-      expect(sp_report.generate).to include(title: title, header: header,
-                                            footer: footer, type: type)
+      title = standard_title 'identity_providers'
+      expect(idp_report.generate).to include(title: title,
+                                             header: header, type: type)
     end
 
     it 'must have title, header and type' do
-      expect(rapid_c_report.generate).to include(title: title, header: header,
-                                                 footer: footer, type: type)
+      title = standard_title 'service_providers'
+      expect(sp_report.generate).to include(title: title,
+                                            header: header, type: type)
     end
 
     it 'must have title, header and type' do
-      expect(service_report.generate).to include(title: title, header: header,
-                                                 footer: footer, type: type)
+      title = standard_title 'rapid_connect_services'
+      expect(rapid_c_report.generate).to include(title: title,
+                                                 header: header, type: type)
+    end
+
+    it 'must have title, header and type' do
+      title = standard_title 'services'
+      expect(service_report.generate).to include(title: title,
+                                                 header: header, type: type)
     end
   end
 
