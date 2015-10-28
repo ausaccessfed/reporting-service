83c7ab0986e91b239c2e32ab726567378ea0c1fd
toomaj <toomaj_b@yahoo.com>
2015-10-28 13:16:49 +1000

    Initial :let(s) for Spec

--- spec/reports/subscriber_registration_report_spec.rb
+++ spec/reports/subscriber_registration_report_spec.rb
@@ -1,4 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe SubscriberRegistrationReport do
+  let(:title) { Faker::Lorem.sentence }
+  let(:header) { [['Name', 'Registration Date']] }
+  let(:footer) { [['Name', 'Registration Date']] }
+  let(:type) { 'subscriber-registrations' }
+
+  let(:org_report) do
+    SubscriberRegistrationReport.new(title, 'organizations')
+  end
+
+  let(:idp_report) do
+    SubscriberRegistrationReport.new(title, 'identity_providers')
+  end
+
+  let(:sp_report) do
+    SubscriberRegistrationReport.new(title, 'service_providers')
+  end
+
+  let(:rapid_c_report) do
+    SubscriberRegistrationReport.new(title, 'rapid_connect_services')
+  end
 end
