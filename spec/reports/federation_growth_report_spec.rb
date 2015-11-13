94425fd09ca8a22d3c00b6ac7b0f5c2ad3d4e9d2
toomaj <toomaj_b@yahoo.com>
2015-11-13 13:55:49 +1000

    Add count_in_range method to examples and cleanup

--- spec/reports/federation_growth_report_spec.rb
+++ spec/reports/federation_growth_report_spec.rb
@@ -9,22 +9,27 @@ RSpec.describe FederationGrowthReport do
       services: 'Services' }
   end
 
-  let(:start) { Time.zone.now - 1.week }
-  let(:finish) { Time.zone.now }
+  let(:start) { (Time.zone.now - 1.week).beginning_of_day }
+  let(:finish) { (Time.zone.now).beginning_of_day }
   let!(:range) { { start: start.xmlschema, end: finish.xmlschema } }
-  let(:int_range) { (0..(finish.to_i - start.to_i)).step(1.day) }
+  let(:full_range_count) { (0..(finish - start).to_i).step(1.day).count }
+  let(:scope_range) { (0..(finish - start).to_i).step(1.day) }
+  let(:type_count) do
+    { organizations: 1, identity_providers: 1, services: 2 }
+  end
 
-  let(:count_in_range) do
-    type_count = { organizations: 1, identity_providers: 1, services: 2 }
-    total_array = [1, 2, 4]
+  let(:type_total) do
+    { organizations: 1, identity_providers: 2, services: 4 }
+  end
+
+  let(:data) { report[:data] }
+
+  def count_in_range(start_index = 0, end_index = full_range_count)
     counter = 0
-
-    int_range.each do |time|
-      stamp = 0
-      type_count.map do |k, val|
-        total = total_array[stamp]
-        expect(report[:data][k][counter]).to match_array([time, total, val])
-        stamp += 1
+    scope_range.each do |time|
+      type_count.each do |k, val|
+        expect(data[k].slice(start_index...end_index)[counter])
+          .to match_array([time, type_total[k], val])
       end
       counter += 1
     end
@@ -43,7 +48,6 @@ RSpec.describe FederationGrowthReport do
   end
 
   subject { FederationGrowthReport.new(title, start, finish) }
-  let(:report) { subject.generate }
 
   shared_examples 'a report which generates growth analytics' do
     context 'growth report when some objects are not included' do
@@ -53,7 +57,7 @@ RSpec.describe FederationGrowthReport do
       end
 
       it 'will not fail if some object types are not existing' do
-        count_in_range
+        expect { subject.generate }.not_to raise_error
       end
     end
   end
@@ -90,7 +94,6 @@ RSpec.describe FederationGrowthReport do
 
   context '#generate report' do
     let(:report) { subject.generate }
-
     it 'output structure should match stacked_report' do
       [:organizations,
        :identity_providers, :services].each do |type|
@@ -131,11 +134,17 @@ RSpec.describe FederationGrowthReport do
     end
 
     context 'with objects deactivated within the range' do
-      let(:midtime) { start + ((finish - start) / 2) }
-      let(:midtime_point) { (finish - midtime).to_i }
-      let(:before_midtime) { (0...(midtime.to_i - start.to_i)).step(1.day) }
-      let(:after_midtime) do
-        ((midtime.to_i - start.to_i)..(finish.to_i - start.to_i)).step(1.day)
+      let(:midtime) do
+        (start + ((finish - start) / 2) + 1.day).beginning_of_day
+      end
+
+      let(:range_before_midtime) do
+        (0...(midtime - start).to_i).step(1.day)
+      end
+
+      let(:range_after_midtime) do
+        ((midtime - start).to_i..(finish - start).to_i)
+          .step(1.day)
       end
 
       before :example do
@@ -147,8 +156,28 @@ RSpec.describe FederationGrowthReport do
           end
       end
 
-      xit 'shoud count objects before deactivation'
-      xit 'shoud not count objects after deactivation'
+      context 'when objects are still active' do
+        let(:scope_range) { range_before_midtime }
+        let(:type_count) do
+          { organizations: 2, identity_providers: 2, services: 4 }
+        end
+
+        let(:type_total) do
+          { organizations: 2, identity_providers: 4, services: 8 }
+        end
+
+        it 'shoud count objects before deactivation' do
+          count_in_range
+        end
+      end
+
+      context 'when objects are deactivated' do
+        let(:scope_range) { range_after_midtime }
+
+        it 'shoud not count objects after deactivation' do
+          count_in_range scope_range.count
+        end
+      end
     end
   end
 end
