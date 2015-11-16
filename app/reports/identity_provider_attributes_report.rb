58133af778197df3ae041a7df5b41e8c6ebb6931
toomaj <toomaj_b@yahoo.com>
2015-11-16 11:26:04 +1000

    Simulate and test :rows output

--- app/reports/identity_provider_attributes_report.rb
+++ app/reports/identity_provider_attributes_report.rb
@@ -1,2 +1,13 @@
 class IdentityProviderAttributesReport < TabularReport
+  report_type 'identity-provider-attributes'
+  header ['Name', 'Core Attributes', 'Optional Attributes']
+  footer
+
+  def initialize
+    super('Identity Provider Attributes')
+  end
+
+  def rows
+    [%w(a a a), %w(b b b), %w(c c c)]
+  end
 end
--- spec/reports/identity_provider_attributes_report_spec.rb
+++ spec/reports/identity_provider_attributes_report_spec.rb
@@ -1,4 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe IdentityProviderAttributesReport do
+  let(:type) { 'identity-provider-attributes' }
+  let(:header) { [['Name', 'Core Attributes', 'Optional Attributes']] }
+  let(:title) { 'Identity Provider Attributes' }
+
+  subject { IdentityProviderAttributesReport.new }
+
+  context 'a tabular repot which lists IdPs attributes' do
+    it 'rows data is an array' do
+      expect(subject.rows).to be_a(Array)
+    end
+
+    it 'includes report :type, :header, :footer' do
+      expect(subject.generate).to include(type: type,
+                                          title: title, header: header)
+    end
+  end
 end
