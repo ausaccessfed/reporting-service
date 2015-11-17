623df8cd437cf5d17ebaf012dc0256e0f01a5ca0
toomaj <toomaj_b@yahoo.com>
2015-11-17 10:00:49 +1000

    Improve federation object shared_example

--- app/models/concerns/federation_object.rb
+++ app/models/concerns/federation_object.rb
@@ -2,6 +2,7 @@ require 'active_support/concern'
 
 module FederationObject
   extend ActiveSupport::Concern
+
   included do
     scope :active, lambda {
       joins(:activations).where(activations: { deactivated_at: nil })
--- spec/support/federation_object.rb
+++ spec/support/federation_object.rb
@@ -1,9 +1,30 @@
 RSpec.shared_examples 'a federation object' do
-  before do
-    create :activation, federation_object: subject
+  context 'when all objects are active' do
+    before do
+      5.times { create :activation, federation_object: subject }
+    end
+
+    it 'should respond to #active' do
+      expect(described_class).to respond_to :active
+      expect(described_class.active.count).to eq(5)
+    end
   end
 
-  it 'should respond to #active' do
-    expect(subject.activations.count).to eq(1)
+  context 'when all objects are deactivated' do
+    before do
+      2.times do
+        create :activation, federation_object: subject
+      end
+
+      2.times do
+        create :activation, :deactivated,
+               federation_object: subject
+      end
+    end
+
+    it 'should respond to #active' do
+      expect(described_class).to respond_to :active
+      expect(described_class.active.count).to eq(2)
+    end
   end
 end
