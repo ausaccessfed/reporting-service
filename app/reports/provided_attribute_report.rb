570a6f93c9d687247455fdc9e98e0ae385cec649
toomaj <toomaj_b@yahoo.com>
2015-11-19 13:14:15 +1000

    Add :supported method ant examples

--- app/reports/provided_attribute_report.rb
+++ app/reports/provided_attribute_report.rb
@@ -3,7 +3,36 @@ class ProvidedAttributeReport < TabularReport
   header %w(Name Supported)
   footer
 
+  def initialize(name)
+    super(name)
+    @name = name
+  end
+
   def rows
-    [%w(name yes)]
+    sorted_idps = identity_providers.sort_by do |idp|
+      idp.name.downcase
+    end
+
+    sorted_idps.map do |idp|
+      yes_or_no = supported idp
+      [idp.name, yes_or_no]
+    end
+  end
+
+  private
+
+  def identity_providers
+    IdentityProvider.joins(:saml_attributes)
+      .preload(:saml_attributes)
+  end
+
+  def supported(idp)
+    names = []
+    idp.saml_attributes.map do |a|
+      names << a.name
+    end
+
+    return 'no' unless names.include?(@name)
+    'yes'
   end
 end
--- spec/reports/provided_attribute_report_spec.rb
+++ spec/reports/provided_attribute_report_spec.rb
@@ -4,17 +4,72 @@ RSpec.describe ProvidedAttributeReport do
   let(:type) { 'provided-attribute-report' }
   let(:header) { [%w(Name Supported)] }
 
-  let(:attribute) { create :saml_attribute }
-  let(:title) { attribute.name }
+  let!(:first_attribute) { create :saml_attribute }
+  let!(:second_attribute) { create :saml_attribute }
 
-  subject { ProvidedAttributeReport.new(title) }
+  let(:identity_provider_01) do
+    create :identity_provider, saml_attributes: [first_attribute]
+  end
 
-  context '#generate' do
+  let(:identity_provider_02) do
+    create :identity_provider, saml_attributes: [second_attribute]
+  end
+
+  let(:identity_provider_03) do
+    create :identity_provider,
+           saml_attributes: [first_attribute, second_attribute]
+  end
+
+  let!(:identity_providers) do
+    { identity_provider_01: identity_provider_01,
+      identity_provider_02: identity_provider_02,
+      identity_provider_03: identity_provider_03 }
+  end
+
+  shared_examples 'an attribute report' do
+    subject { ProvidedAttributeReport.new(title) }
+
     let(:report) { subject.generate }
 
     it 'produces title, header and type' do
       expect(report).to include(header: header,
                                 type: type, title: title)
+    end
+
+    it 'should generate an array of report rows' do
+      identity_providers.each do |_k, v|
+        expect(report[:rows]).to include([v.name, anything])
+      end
+    end
+
+    it 'determines whether idp is supported or not' do
+      supported_idps.each do |k, v|
+        expect(report[:rows]).to include([identity_providers[k].name, v])
+      end
+    end
+  end
+
+  context '#generate' do
+    context 'first attribute' do
+      let(:title) { first_attribute.name }
+      let(:supported_idps) do
+        { identity_provider_01: 'yes',
+          identity_provider_02: 'no',
+          identity_provider_03: 'yes' }
+      end
+
+      it_behaves_like 'an attribute report'
+    end
+
+    context 'second attribute' do
+      let(:title) { second_attribute.name }
+      let(:supported_idps) do
+        { identity_provider_01: 'no',
+          identity_provider_02: 'yes',
+          identity_provider_03: 'yes' }
+      end
+
+      it_behaves_like 'an attribute report'
     end
   end
 end
