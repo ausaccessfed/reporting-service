65d36c05ab7e5540b371a7fa017271cc88e0d8d5
toomaj <toomaj_b@yahoo.com>
2015-11-19 10:15:11 +1000

    some initial methods and examples

--- app/reports/provided_attribute_report.rb
+++ app/reports/provided_attribute_report.rb
@@ -1,2 +1,9 @@
 class ProvidedAttributeReport < TabularReport
+  report_type 'provided-attribute-report'
+  header %w(Name Supported)
+  footer
+
+  def rows
+    [%w(name yes)]
+  end
 end
--- spec/reports/provided_attribute_report_spec.rb
+++ spec/reports/provided_attribute_report_spec.rb
@@ -1,6 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe ProvidedAttributeReport do
-  let(:title) { 'provide-attribute-report' }
+  let(:type) { 'provided-attribute-report' }
   let(:header) { [%w(Name Supported)] }
+
+  let(:attribute) { create :saml_attribute }
+  let(:title) { attribute.name }
+
+  subject { ProvidedAttributeReport.new(title) }
+
+  context '#generate' do
+    let(:report) { subject.generate }
+
+    it 'produces title, header and type' do
+      expect(report).to include(header: header,
+                                type: type, title: title)
+    end
+  end
 end
