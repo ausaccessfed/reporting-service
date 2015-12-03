896b191c8c12d6e421762c347a9eeeade1a653c5
toomaj <toomaj_b@yahoo.com>
2015-12-03 11:02:29 +1000

    Add factories and validation test for DiscoveryServiceEvent

--- spec/factories/discovery_service_events.rb
+++ spec/factories/discovery_service_events.rb
@@ -1,4 +1,20 @@
 FactoryGirl.define do
   factory :discovery_service_event do
+    transient { domain { Faker::Internet.domain_name } }
+
+    user_agent { 'Mozilla/5.0' }
+    ip { Faker::Internet.ip_v4_address }
+    initiating_sp { "https://#{domain}.aaf.edu.au/shibboleth" }
+    group { Faker::Lorem.word }
+    unique_id { Faker::Internet.password(10) }
+    phase { 'request' }
+    timestamp { Faker::Time.between(40.days.ago, Time.zone.now, :all) }
+
+    trait :response do
+      transient { domain { Faker::Internet.domain_name } }
+      phase { 'response' }
+      selection_method { %w(manual cookie).sample }
+      selected_idp { "https://#{domain}.test.aaf.edu.au/idp/shibboleth" }
+    end
   end
 end
