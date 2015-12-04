121b281d3f575055ffd3472bf8dfa3c64c7619c7
toomaj <toomaj_b@yahoo.com>
2015-12-04 16:14:09 +1000

    improve range method and descovery_service_events factory

--- app/reports/federated_sessions_report.rb
+++ app/reports/federated_sessions_report.rb
@@ -13,7 +13,7 @@ class FederatedSessionsReport < TimeSeriesReport
     super(title, start, finish)
     @start = start
     @finish = finish
-    @steps = steps.to_f.round(2)
+    @steps = steps
   end
 
   private
@@ -21,10 +21,27 @@ class FederatedSessionsReport < TimeSeriesReport
   def range
     start = @start
     finish = @finish
-    (0..(finish.to_i - start.to_i)).step(@steps.minutes)
+    ((@steps.minutes.to_i)..(finish - start).to_i).step(@steps.minutes)
   end
 
   def data
-    { sessions: range.map { |t| [t, 1] } }
+    objects = DiscoveryServiceEvent
+              .where('timestamp >= ? AND timestamp <= ? AND phase LIKE ?',
+                     @start, @finish, 'response').pluck(:timestamp)
+
+    index = 0
+    range.each_with_object(sessions: []) do |time, data|
+      rate = average_rate time, index, objects
+      index = time + @steps.minutes
+      data[:sessions] << [time, rate]
+    end
+  end
+
+  def average_rate(time, index, sessions)
+    data = sessions.select do |s|
+      (s >= (@start + index)) && (s <= (@start + time))
+    end
+
+    (data.count / @steps).round(1)
   end
 end
--- spec/reports/federated_sessions_report_spec.rb
+++ spec/reports/federated_sessions_report_spec.rb
@@ -6,14 +6,12 @@ RSpec.describe FederatedSessionsReport do
   let(:units) { '' }
   let(:labels) { { y: '', sessions: 'Rate/m' } }
 
-  let(:start) { Time.zone.now.beginning_of_day }
-  let(:finish) { (start + 2.hour) }
-  let(:steps) { 10.5 }
+  let(:start) { 2.days.ago.beginning_of_day }
+  let(:finish) { 1.day.ago.beginning_of_day }
+  let(:steps) { 10 }
   let!(:range) { { start: start.xmlschema, end: finish.xmlschema } }
-  let(:scope_range) { (0..(finish.to_i - start.to_i)).step(steps) }
-
-  before :context do
-    create_list :discovery_service_event, 100
+  let(:scope_range) do
+    ((steps.minutes.to_i)..(finish - start).to_i).step(steps.minutes)
   end
 
   subject { FederatedSessionsReport.new(start, finish, steps) }
@@ -27,14 +25,30 @@ RSpec.describe FederatedSessionsReport do
     end
   end
 
-  context 'a federated sessions report' do
+  context 'when objects are sessions' do
+    before :context do
+      create_list :discovery_service_event, 20, :response,
+                  timestamp: 1.day.ago.beginning_of_day
+    end
+
     it 'includes title' do
       expect(report).to include(title: title, units: units,
                                 labels: labels, range: range)
     end
 
-    it 'report should be generated within a range' do
-      scope_range
+    it 'sessions are response types generated within given range' do
+      expect_in_range
+    end
+  end
+
+  context 'when sessions are not responded' do
+    before do
+      create_list :discovery_service_event, 20,
+                  timestamp: 1.day.ago.beginning_of_day
+    end
+
+    it 'should not count any sessions' do
+      expect_in_range
     end
   end
 end
