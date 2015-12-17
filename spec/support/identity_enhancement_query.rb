86144e36f8830f5dcffac733e1ead7a929199b0b
Shaun Mangelsdorf <s.mangelsdorf@gmail.com>
2015-12-17 15:48:05 +1000

    Add shared stub for IdE queries

---
+++ spec/support/identity_enhancement_query.rb
@@ -0,0 +1,10 @@
+module IdentityEnhancementQuery
+  def stub_ide(shared_token:, entitlements: [], **)
+    host = Rails.application.config.reporting_service.ide[:host]
+    url =  "https://#{host}/api/subjects/#{shared_token}/attributes"
+
+    attrs = entitlements.map { |v| { name: 'eduPersonEntitlement', value: v } }
+    stub_request(:get, url)
+      .to_return(status: 200, body: JSON.generate(attributes: attrs))
+  end
+end
