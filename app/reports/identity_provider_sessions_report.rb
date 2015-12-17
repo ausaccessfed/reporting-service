e49373c300e3c6285a8062cc3557e41b1ba41589
toomaj <toomaj_b@yahoo.com>
2015-12-18 08:58:58 +1000

    Add logics to idp_sessions_report

--- app/reports/identity_provider_sessions_report.rb
+++ app/reports/identity_provider_sessions_report.rb
@@ -3,19 +3,49 @@ class IdentityProviderSessionsReport < TimeSeriesReport
 
   y_label ''
 
-  series sessions: 'sessions'
+  series sessions: 'Rate/h'
 
   units ''
 
-  def initialize(start, finish)
-    title = 'Identity Provider Sessions'
+  def initialize(entity_id, start, finish, steps)
+    @identity_provider = IdentityProvider.find_by(entity_id: entity_id)
+    title = "Identity Provider Sessions for #{@identity_provider.name}"
 
     super(title, start: start, end: finish)
+    @start = start
+    @finish = finish
+    @steps = steps
   end
 
   private
 
+  def range
+    (0..(@finish - @start).to_i).step(@steps.hours)
+  end
+
   def data
-    { sessions: [[1, 0]] }
+    sessions = idp_sessions(@identity_provider.id)
+
+    report = average_rate sessions
+    range.each_with_object(sessions: []) do |t, data|
+      average = report[t] ? (report[t].to_f / @steps).round(1) : 0.0
+      data[:sessions] << [t, average]
+    end
+  end
+
+  def average_rate(sessions)
+    sessions.each_with_object({}) do |session, data|
+      offset = session - @start
+      point = offset - (offset % @steps.hours)
+      (data[point.to_i] ||= 0) << data[point.to_i] += 1
+    end
+  end
+
+  def idp_sessions(idp_id)
+    query = 'identity_provider_id = ? AND timestamp >= ?'\
+            'AND timestamp <= ? AND phase LIKE ?'
+
+    DiscoveryServiceEvent
+      .where(query, idp_id, @start, @finish, 'response').pluck(:timestamp)
   end
 end
--- spec/reports/identity_provider_sessions_report_spec.rb
+++ spec/reports/identity_provider_sessions_report_spec.rb
@@ -4,34 +4,116 @@ RSpec.describe IdentityProviderSessionsReport do
   around { |spec| Timecop.freeze { spec.run } }
 
   let(:type) { 'identity-provider-sessions' }
-  let(:title) { 'Identity Provider Sessions' }
+  let(:title) { 'Identity Provider Sessions for' }
 
-  let(:labels) { { y: '', sessions: 'sessions' } }
+  let(:labels) { { y: '', sessions: 'Rate/h' } }
   let(:units) { '' }
 
   let!(:start) { 10.days.ago.beginning_of_day }
   let!(:finish) { 1.day.ago.end_of_day }
 
   let!(:range) { { start: start.xmlschema, end: finish.xmlschema } }
+  let(:steps) { 5 }
 
-  let(:identity_provider) { create :identity_provider }
-  let(:service_provider) { create :service_provider }
+  let(:scope_range) do
+    (0..(finish - start).to_i).step(steps.hours.to_i)
+  end
 
-  subject { IdentityProviderSessionsReport.new(start, finish) }
+  let(:idp) { create :identity_provider }
+  let(:idp_2) { create :identity_provider }
+  let(:sp) { create :service_provider }
+
+  subject do
+    IdentityProviderSessionsReport.new(idp.entity_id, start, finish, steps)
+  end
 
   let(:report) { subject.generate }
   let(:data) { report[:data] }
 
+  def expect_in_range
+    [*scope_range].each_with_index do |t, index|
+      expect(data[:sessions][index]).to match_array([t, value])
+    end
+  end
+
   context 'when events are sessions with response' do
     before do
       create_list :discovery_service_event, 20, :response,
-                  identity_provider: identity_provider,
-                  service_provider: service_provider
+                  identity_provider: idp,
+                  service_provider: sp
     end
 
+    let(:value) { anything }
+
     it 'should include title, units, labels and range' do
-      expect(report).to include(title: title, units: units,
+      output_title = title + ' ' + idp.name
+      expect(report).to include(title: output_title, units: units,
                                 labels: labels, range: range)
+    end
+
+    it 'sessions should be generated within given range' do
+      expect_in_range
+    end
+  end
+
+  context 'when IdP sessions are not yet completed' do
+    before do
+      create_list :discovery_service_event, 2,
+                  service_provider: sp
+    end
+
+    let(:value) { 0.0 }
+
+    it 'should not count IdP sessions' do
+      expect_in_range
+    end
+  end
+
+  context 'when IdPs have sessions' do
+    before :example do
+      create_list :discovery_service_event, 5, :response,
+                  identity_provider: idp,
+                  service_provider: sp,
+                  timestamp: start
+
+      create_list :discovery_service_event, 10, :response,
+                  identity_provider: idp,
+                  service_provider: sp,
+                  timestamp: 2.days.ago.beginning_of_day
+
+      create_list :discovery_service_event, 9, :response,
+                  identity_provider: idp,
+                  service_provider: sp,
+                  timestamp: finish
+
+      create_list :discovery_service_event, 5, :response,
+                  identity_provider: idp_2,
+                  service_provider: sp,
+                  timestamp: 3.days.ago.beginning_of_day
+    end
+
+    it 'average should be 1.0 for 5 IdP sessions during first 5 hours' do
+      time = [*scope_range].first
+      expect(data[:sessions]).to include([time, 1.0])
+    end
+
+    it 'should contain 2.0 IdP sessions/h during first 5 hours of 2 days ago' do
+      expect(data[:sessions]).to include([684_000, 2.0])
+    end
+
+    it 'average should be 0.0 for no IdP sessions during 2nd last 5 hours' do
+      time = [*scope_range][-2]
+      expect(data[:sessions]).to include([time, 0.0])
+    end
+
+    it 'average should be 1.8 for 9 IdP sessions during last 5 hours' do
+      time = [*scope_range].last
+      expect(data[:sessions]).to include([time, 1.8])
+    end
+
+    it 'report should not count sessions of an other IdP' do
+      time = [*scope_range][-4]
+      expect(data[:sessions]).to include([time, 0.0])
     end
   end
 end
