def324ccfd7a1d4b3c5c0b47dca6dc6d0199073b
toomaj <toomaj_b@yahoo.com>
2015-12-17 12:30:28 +1000

    Improvemnt in demand_average_report

--- app/reports/daily_demand_report.rb
+++ app/reports/daily_demand_report.rb
@@ -13,7 +13,6 @@ class DailyDemandReport < TimeSeriesReport
     super(title)
     @start = start.beginning_of_day
     @finish = finish.end_of_day
-    @days_count = (@finish - @start) / 86_400
   end
 
   private
@@ -25,18 +24,21 @@ class DailyDemandReport < TimeSeriesReport
 
     report = demand_average_report sessions
 
-    (0..86_340).step(60).each_with_object(sessions: []) do |t, data|
-      average = report[t] ? (report[t].to_f / @days_count).round(1) : 0.0
+    (0..86_340).step(1.minute).each_with_object(sessions: []) do |t, data|
+      average = report[t] ? (report[t].to_f / days_count).round(1) : 0.0
 
       data[:sessions] << [t, average]
     end
   end
 
+  def days_count
+    (@start.to_i..@finish.to_i).step(1.day).count
+  end
+
   def demand_average_report(sessions)
     sessions.each_with_object({}) do |session, data|
-      offset = session - @start
-      increment = offset - offset % 86_400
-      point = offset - (offset % 60) - increment.to_i
+      t = (session - session.beginning_of_day).to_i
+      point = t - t % 1.minute
       (data[point.to_i] ||= 0) << data[point.to_i] += 1
     end
   end
