e22a49dfbd22f56a018071529558aa545f1587a9
Shaun Mangelsdorf <s.mangelsdorf@gmail.com>
2015-12-17 15:13:42 +1000

    Remove placeholder ReportsController

--- app/controllers/reports_controller.rb
+++
@@ -1,8 +0,0 @@
-class ReportsController < ApplicationController
-  def show
-    public_action
-    @start = 1.week.ago.utc.beginning_of_day
-    @finish = Time.now.utc.beginning_of_day
-    @data = RandomTabularDataReport.new('Test Report Randomness!').generate
-  end
-end
