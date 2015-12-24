faa4fa39b4aa03bb3de525bf399a41a00fbdf4bf
toomaj <toomaj_b@yahoo.com>
2015-12-24 10:33:37 +1000

    use Capybara test cases

--- spec/features/public_reports_spec.rb
+++ spec/features/public_reports_spec.rb
@@ -10,16 +10,31 @@ RSpec.feature 'Public Reports' do
     RapidRack::TestAuthenticator.jwt = create(:jwt, aaf_attributes: attrs)
 
     stub_ide(shared_token: user.shared_token)
-
-    visit '/auth/login'
-    click_button 'Login'
   end
 
-  scenario 'viewing the Federation Growth Report' do
-    expect(current_path).to eq('/dashboard')
+  feature 'requesting /auth/login path' do
+    background do
+      visit '/auth/login'
+      click_button 'Login'
+    end
 
-    click_link('Federation Growth Report')
-    expect(current_path).to eq('/public_reports/federation_growth')
-    expect(page).to have_css('svg.federation-growth')
+    scenario 'viewing the Dashboard and then Federation Growth Report' do
+      expect(current_path).to eq('/dashboard')
+
+      click_link('Federation Growth Report')
+      expect(current_path).to eq('/public_reports/federation_growth')
+      expect(page).to have_css('svg.federation-growth')
+    end
+  end
+
+  feature 'requesting /public_reports/federation_growth path' do
+    background do
+      visit '/public_reports/federation_growth'
+      click_button 'Login'
+    end
+
+    scenario 'viewing the Federation Growth Report directly' do
+      expect(current_path).to eq('/public_reports/federation_growth')
+    end
   end
 end
