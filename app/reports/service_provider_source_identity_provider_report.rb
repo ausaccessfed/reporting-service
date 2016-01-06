a924512a7c9994ca66dc2e12a53e402167bf1991
toomaj <toomaj_b@yahoo.com>
2016-01-06 11:25:34 +1000

    Implement report output

--- app/reports/service_provider_source_identity_provider_report.rb
+++ app/reports/service_provider_source_identity_provider_report.rb
@@ -1,17 +1,24 @@
 class ServiceProviderSourceIdentityProviderReport < TabularReport
+  prepend TimeSeriesSharedMethods
+
   report_type 'service-provider-source-identity-providers'
   header ['IdP Name', 'Total']
   footer
 
-  def initialize(entity_id, _start, _finish)
+  def initialize(entity_id, start, finish)
     @service_provider = ServiceProvider.find_by(entity_id: entity_id)
     title = "SP Source Identity Provider Report for #{@service_provider.name}"
+    @start = start
+    @finish = finish
+
     super(title)
   end
 
   private
 
   def rows
-    [['', '']]
+    sp_sessions.preload(:identity_provider)
+      .group_by(&:identity_provider)
+      .map { |idp, val| [idp.name, val.count.to_s] }
   end
 end
--- spec/reports/service_provider_source_identity_provider_report_spec.rb
+++ spec/reports/service_provider_source_identity_provider_report_spec.rb
@@ -11,6 +11,9 @@ RSpec.describe ServiceProviderSourceIdentityProviderReport do
   let(:finish) { Time.zone.now.end_of_day }
 
   let(:sp) { create :service_provider }
+  let(:idp_01) { create :identity_provider }
+  let(:idp_02) { create :identity_provider }
+  let(:idp_03) { create :identity_provider }
 
   subject do
     ServiceProviderSourceIdentityProviderReport.new(sp.entity_id, start, finish)
@@ -23,6 +26,36 @@ RSpec.describe ServiceProviderSourceIdentityProviderReport do
       output_title = "#{title} #{sp.name}"
       expect(report).to include(type: type,
                                 title: output_title, header: header)
+    end
+  end
+
+  context '#generate' do
+    before do
+      create_list :discovery_service_event, 20, :response,
+                  service_provider: sp,
+                  identity_provider: idp_01
+
+      create_list :discovery_service_event, 5, :response,
+                  service_provider: sp,
+                  identity_provider: idp_02
+
+      create_list :discovery_service_event, 10,
+                  service_provider: sp,
+                  identity_provider: idp_03,
+                  timestamp: 20.day.ago.beginning_of_day
+    end
+
+    it 'creates report :rows with number of related IdPs and IdP names' do
+      idp_name_01 = idp_01.name
+      idp_name_02 = idp_02.name
+
+      expect(report[:rows]).to include([idp_name_01, '20'])
+      expect(report[:rows]).to include([idp_name_02, '5'])
+    end
+
+    it 'report should not include sessions out of range' do
+      idp_name_03 = idp_03.name
+      expect(report[:rows]).not_to include([idp_name_03, anything])
     end
   end
 end
