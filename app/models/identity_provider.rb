4d2fbb8cabd6adb0d1edbf9495cf259c0e442e60
Shaun Mangelsdorf <s.mangelsdorf@gmail.com>
2016-01-07 14:18:12 +1000

    Add missing organization relationship to models

--- app/models/identity_provider.rb
+++ app/models/identity_provider.rb
@@ -1,6 +1,8 @@
 class IdentityProvider < ActiveRecord::Base
   include FederationObject
 
+  belongs_to :organization
+
   has_many :activations, as: :federation_object
   has_many :identity_provider_saml_attributes
   has_many :saml_attributes,
--- spec/factories/service_provider.rb
+++ spec/factories/service_provider.rb
@@ -1,5 +1,7 @@
 FactoryGirl.define do
   factory :service_provider do
+    organization
+
     transient do
       sequence(:domain) do |s|
         Faker::Internet.domain_name + s.to_s
