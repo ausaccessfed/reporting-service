4c5aa9b1c0c7115f67fa26edde29f86833b2c6fa
toomaj <toomaj_b@yahoo.com>
2016-01-08 09:04:57 +1000

    refactor route tests

--- spec/routing/compliance_reports_routing_spec.rb
+++ spec/routing/compliance_reports_routing_spec.rb
@@ -1,33 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe ComplianceReportsController, type: :routing do
-  describe 'get /compliance_reports/service_provider/compatibility_report' do
-    let(:action) { 'compliance_reports#service_provider_compatibility_report' }
+  let(:controller) { 'compliance_reports' }
 
+  shared_examples 'get request' do
     subject do
-      { get: '/compliance_reports/service_provider/compatibility_report' }
+      { get: controller + path }
     end
 
-    it { is_expected.to route_to(action) }
+    it { is_expected.to route_to(controller + action) }
   end
 
-  describe 'post /compliance_reports/service_provider/compatibility_report' do
-    let(:action) { 'compliance_reports#service_provider_compatibility_report' }
-
+  shared_examples 'post request' do
     subject do
-      { post: '/compliance_reports/service_provider/compatibility_report' }
+      { post: controller + path }
     end
 
-    it { is_expected.to route_to(action) }
+    it { is_expected.to route_to(controller + action) }
   end
 
-  describe 'get /compliance_reports/identity_provider/attributes_report' do
-    let(:action) { 'compliance_reports#identity_provider_attributes_report' }
+  describe 'get /service_provider/compatibility_report' do
+    let(:action) { '#service_provider_compatibility_report' }
+    let(:path) { '/service_provider/compatibility_report' }
+    it_behaves_like 'get request'
+  end
 
-    subject do
-      { get: '/compliance_reports/identity_provider/attributes_report' }
-    end
+  describe 'get /identity_provider/attributes_report' do
+    let(:action) { '#identity_provider_attributes_report' }
+    let(:path) { '/identity_provider/attributes_report' }
+    it_behaves_like 'get request'
+  end
 
-    it { is_expected.to route_to(action) }
+  describe 'post /service_provider/compatibility_report' do
+    let(:action) { '#service_provider_compatibility_report' }
+    let(:path) { '/service_provider/compatibility_report' }
+    it_behaves_like 'post request'
   end
 end
