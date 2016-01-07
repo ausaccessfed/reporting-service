27a0281e9a9a3cfcd5d1b682fce11519c316181c
Shaun Mangelsdorf <s.mangelsdorf@gmail.com>
2016-01-07 15:21:44 +1000

    Add missing inverse relationships
    Per review comments these were accidentally omitted.

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -2,6 +2,9 @@ class Organization < ActiveRecord::Base
   include FederationObject
 
   has_many :activations, as: :federation_object
+  has_many :identity_providers
+  has_many :service_providers
+  has_many :rapid_connect_services
 
   valhammer
 
