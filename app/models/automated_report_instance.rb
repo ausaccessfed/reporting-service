668eca59d03b718ccd739a81c68abe144966b393
Shaun Mangelsdorf <s.mangelsdorf@gmail.com>
2016-01-12 11:28:57 +1000

    Add AutomatedReportInstance#materialize method

--- app/models/automated_report_instance.rb
+++ app/models/automated_report_instance.rb
@@ -5,6 +5,13 @@ class AutomatedReportInstance < ActiveRecord::Base
 
   validate :time_must_be_utc_midnight
 
+  delegate :interval, to: :automated_report
+
+  def materialize
+    args = [automated_report.target, *report_range, step_width].compact
+    automated_report.report_class.constantize.new(*args)
+  end
+
   private
 
   def time_must_be_utc_midnight
@@ -13,4 +20,44 @@ class AutomatedReportInstance < ActiveRecord::Base
 
     errors.add(:range_start, 'must be midnight, UTC')
   end
+
+  def report_range
+    return nil unless needs_range?
+
+    n = 1
+    n = 3 if interval.quarterly?
+    n = 12 if interval.yearly?
+
+    range_end = n.months.since(range_start)
+    [range_start, range_end]
+  end
+
+  def step_width
+    return nil unless needs_step_width?
+
+    1
+  end
+
+  REPORTS_THAT_NEED_RANGE = %w(
+    FederationGrowthReport DailyDemandReport FederatedSessionsReport
+    IdentityProviderDailyDemandReport IdentityProviderDestinationServicesReport
+    IdentityProviderSessionsReport
+    ServiceProviderDailyDemandReport ServiceProviderSessionsReport
+    ServiceProviderSourceIdentityProvidersReport
+  )
+
+  def needs_range?
+    REPORTS_THAT_NEED_RANGE.include?(automated_report.report_class)
+  end
+
+  REPORTS_THAT_NEED_STEP_WIDTH = %w(
+    FederatedSessionsReport IdentityProviderSessionsReport
+    ServiceProviderSessionsReport
+  )
+
+  def needs_step_width?
+    REPORTS_THAT_NEED_STEP_WIDTH.include?(automated_report.report_class)
+  end
+
+  private_constant :REPORTS_THAT_NEED_RANGE, :REPORTS_THAT_NEED_STEP_WIDTH
 end
