b472bfb373d9d3e96f4a4ffff61e79a7015d9b95
toomaj <toomaj_b@yahoo.com>
2016-01-25 12:05:55 +1000

    DRY subscriber report controllers example

--- spec/controllers/identity_provider_reports_controller_spec.rb
+++ spec/controllers/identity_provider_reports_controller_spec.rb
@@ -1,75 +1,14 @@
 require 'rails_helper'
+require 'controllers/subscriber_reports_controller'
 
 RSpec.describe IdentityProviderReportsController, type: :controller do
-  def run_get
-    get report_path
-  end
+  let(:prefix) { 'identity' }
 
-  def run_post
-    post report_path,
-         entity_id: object.entity_id,
-         start: 1.year.ago.utc, end: Time.now.utc
-  end
-
-  shared_examples 'an Identity Provider Report Controller' do
-    let(:organization) { create :organization }
-    let(:object) { create :identity_provider, organization: organization }
-    let(:bad_object) { create :identity_provider }
-
-    let(:user) do
-      create :subject, :authorized,
-             permission:
-             "objects:organization:#{organization.identifier}:report"
-    end
-
-    before do
-      session[:subject_id] = user.try(:id)
-      create :activation, federation_object: object
-      create :activation, federation_object: bad_object
-    end
-
-    context 'with no user' do
-      let(:user) { nil }
-
-      it 'requires authentication' do
-        run_get
-        expect(response).to redirect_to('/auth/login')
-      end
-    end
-
-    context 'with user' do
-      it 'assigns only permitted IdP to the IdPs list' do
-        run_get
-        expect(assigns[:entities]).to include(object)
-        expect(assigns[:entities]).not_to include(bad_object)
-      end
-    end
-
-    context 'generate report' do
-      it 'assigns only permitted IdP to the IdPs list' do
-        run_post
-        expect(assigns[:data]).to be_a(String)
-        data = JSON.parse(assigns[:data], symbolize_names: true)
-        expect(data[:type]).to eq(template)
-      end
-    end
-  end
-
-  context 'Identity Provider Sessions Report' do
-    let(:report_path) { :sessions_report }
-    let(:template) { 'identity-provider-sessions' }
-    it_behaves_like 'an Identity Provider Report Controller'
-  end
-
-  context 'Identity Provider Daily Demand Report' do
-    let(:report_path) { :daily_demand_report }
-    let(:template) { 'identity-provider-daily-demand' }
-    it_behaves_like 'an Identity Provider Report Controller'
-  end
+  include_context 'a Subscriber Report'
 
   context 'Identity Provider Destination Services' do
     let(:report_path) { :destination_services_report }
     let(:template) { 'identity-provider-destination-services' }
-    it_behaves_like 'an Identity Provider Report Controller'
+    it_behaves_like 'Report Controller'
   end
 end
--- spec/controllers/service_provider_reports_controller_spec.rb
+++ spec/controllers/service_provider_reports_controller_spec.rb
@@ -1,75 +1,14 @@
 require 'rails_helper'
+require 'controllers/subscriber_reports_controller'
 
 RSpec.describe ServiceProviderReportsController, type: :controller do
-  def run_get
-    get report_path
-  end
+  let(:prefix) { 'service' }
 
-  def run_post
-    post report_path,
-         entity_id: object.entity_id,
-         start: 1.year.ago.utc, end: Time.now.utc
-  end
+  include_context 'a Subscriber Report'
 
-  shared_examples 'a Service Provider Report Controller' do
-    let(:organization) { create :organization }
-    let(:object) { create :service_provider, organization: organization }
-    let(:bad_object) { create :service_provider }
-
-    let(:user) do
-      create :subject, :authorized,
-             permission:
-             "objects:organization:#{organization.identifier}:report"
-    end
-
-    before do
-      session[:subject_id] = user.try(:id)
-      create :activation, federation_object: object
-      create :activation, federation_object: bad_object
-    end
-
-    context 'with no user' do
-      let(:user) { nil }
-
-      it 'requires authentication' do
-        run_get
-        expect(response).to redirect_to('/auth/login')
-      end
-    end
-
-    context 'with user' do
-      it 'assigns only permitted SPs to the SPs list' do
-        run_get
-        expect(assigns[:entities]).to include(object)
-        expect(assigns[:entities]).not_to include(bad_object)
-      end
-    end
-
-    context 'generate report' do
-      it 'assigns only permitted SPs to the SPs list' do
-        run_post
-        expect(assigns[:data]).to be_a(String)
-        data = JSON.parse(assigns[:data], symbolize_names: true)
-        expect(data[:type]).to eq(template)
-      end
-    end
-  end
-
-  context 'Service Provider Sessions Report' do
-    let(:report_path) { :sessions_report }
-    let(:template) { 'service-provider-sessions' }
-    it_behaves_like 'a Service Provider Report Controller'
-  end
-
-  context 'Service Provider Daily Demand Report' do
-    let(:report_path) { :daily_demand_report }
-    let(:template) { 'service-provider-daily-demand' }
-    it_behaves_like 'a Service Provider Report Controller'
-  end
-
-  context 'Service Provider Daily Demand Report' do
+  context 'Service Provider Source Identity Providers' do
     let(:report_path) { :source_identity_providers_report }
     let(:template) { 'service-provider-source-identity-providers' }
-    it_behaves_like 'a Service Provider Report Controller'
+    it_behaves_like 'Report Controller'
   end
 end
