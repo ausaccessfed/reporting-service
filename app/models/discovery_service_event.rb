0504eb39ae2df7763f4995efe6c1295890d67ab8
Shaun Mangelsdorf <s.mangelsdorf@gmail.com>
2016-01-28 11:36:49 +1000

    Repair test failures by adding relationships back

--- app/models/discovery_service_event.rb
+++ app/models/discovery_service_event.rb
@@ -1,6 +1,14 @@
 class DiscoveryServiceEvent < ActiveRecord::Base
   valhammer
 
+  belongs_to :identity_provider,
+             foreign_key: :selected_idp,
+             primary_key: :entity_id
+
+  belongs_to :service_provider,
+             foreign_key: :initiating_sp,
+             primary_key: :entity_id
+
   scope :within_range, lambda { |start, finish|
     where(arel_table[:timestamp].gteq(start)
       .and(arel_table[:timestamp].lteq(finish)))
