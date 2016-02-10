6b4998224123bc5c8530cceb0f76d4647252d5eb
toomaj <toomaj_b@yahoo.com>
2016-02-11 08:32:15 +1000

    add jQuery table filter

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,3 +3,16 @@
 //= require 'reporting'
 //= require 'render_report'
 //= require jq_validations.js
+
+$(document).ready(function() {
+  (function($) {
+    $('#filter').keyup(function() {
+      var text = new RegExp($(this).val(), 'i');
+
+      $('.filtertable tr').hide();
+      $('.filtertable tr').filter(function() {
+          return text.test($(this).text());
+      }).show();
+    })
+  }($));
+});
