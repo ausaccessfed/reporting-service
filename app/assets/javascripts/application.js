01ffd7c85883b2ae68c8b15855b1b9841a345696
toomaj <toomaj_b@yahoo.com>
2016-02-11 14:21:10 +1000

    add tooltip

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,16 +14,18 @@ $(document).ready(function() {
           return text.test($(this).text());
       }).show();
     })
+
+    $('#delete-confirm').on('show', function() {
+    var $submit = $(this).find('.btn-danger'),
+        href = $submit.attr('href');
+      $submit.attr('href', href.replace('pony', $(this).data('id')));
+    });
+
+    $('.delete-confirm').click(function(e) {
+      e.preventDefault();
+      $('#delete-confirm').data('id', $(this).data('id')).modal('show');
+    });
+
+    $('[data-toggle="tooltip"]').tooltip();
   }($));
-
-  $('#delete-confirm').on('show', function() {
-  var $submit = $(this).find('.btn-danger'),
-      href = $submit.attr('href');
-    $submit.attr('href', href.replace('pony', $(this).data('id')));
-  });
-
-  $('.delete-confirm').click(function(e) {
-    e.preventDefault();
-    $('#delete-confirm').data('id', $(this).data('id')).modal('show');
-  });
 });
