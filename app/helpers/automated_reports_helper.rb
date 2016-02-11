3d54fce318dacc36f94525c945238352b382b6dc
toomaj <toomaj_b@yahoo.com>
2016-02-11 10:05:42 +1000

    kill qn+1

--- app/helpers/automated_reports_helper.rb
+++ app/helpers/automated_reports_helper.rb
@@ -3,9 +3,11 @@ module AutomatedReportsHelper
     return target.titleize if type.eql?('SubscriberRegistrationsReport')
     return 'Whole Federation' unless target_classes.include?(type)
 
+    objects = target_classes[type]
     is_attribute = target_classes[type].to_s.eql?('SAMLAttribute')
-    field = is_attribute ? :name : :entity_id
-    target_classes[type].find_by(field => target).name
+
+    return fin_by_name(objects, target) if is_attribute
+    find_by_entity_id(objects, target)
   end
 
   def target_classes
@@ -19,5 +21,13 @@ module AutomatedReportsHelper
       'ServiceProviderDailyDemandReport' => ServiceProvider,
       'ServiceProviderSessionsReport' => ServiceProvider,
       'ServiceProviderSourceIdentityProvidersReport' => ServiceProvider }
+  end
+
+  def find_by_entity_id(objects, target)
+    objects.all.detect { |s| s.entity_id.eql? target }.name
+  end
+
+  def fin_by_name(objects, target)
+    objects.all.detect { |s| s.name.eql? target }.name
   end
 end
