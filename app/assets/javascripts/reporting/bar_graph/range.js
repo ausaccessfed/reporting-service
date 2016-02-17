d5a454788879d1c9ae06e905eab77f5b8302f39f
Russell Ianniello <russell.ianniello@aaf.edu.au>
2016-02-17 14:45:53 +1000

    Max attribute determined using ints (not strings)

--- app/assets/javascripts/reporting/bar_graph/range.js
+++ app/assets/javascripts/reporting/bar_graph/range.js
@@ -1,7 +1,7 @@
 reporting.barGraph.range = function (report, barDataIndex) {
   var maxAttributeCount = d3.max(report.rows, function (attributes) {
-    var core = attributes[barDataIndex.core];
-    var optional = attributes[barDataIndex.optional];
+    var core = parseInt(attributes[barDataIndex.core]);
+    var optional = parseInt(attributes[barDataIndex.optional]);
     return d3.max([optional, core]);
   });
 
