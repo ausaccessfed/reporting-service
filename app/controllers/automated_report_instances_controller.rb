6dbd5fe4db9a8b0dee9fc16337524c50e3f40d31
toomaj <toomaj_b@yahoo.com>
2016-02-18 15:04:50 +1000

    refactor instacnes controller

--- app/controllers/automated_report_instances_controller.rb
+++ app/controllers/automated_report_instances_controller.rb
@@ -3,7 +3,7 @@ class AutomatedReportInstancesController < ApplicationController
 
   def show
     @instance = instance
-    report = generate_report
+    report = @instance.materialize.generate
     @data = JSON.generate(report)
   end
 
@@ -16,60 +16,6 @@ class AutomatedReportInstancesController < ApplicationController
   def automated_report
     instance.automated_report
   end
-
-  def interval
-    automated_report.interval
-  end
-
-  def range_start
-    instance.range_start
-  end
-
-  def generate_report
-    args = [automated_report.target, *report_range, step_width].compact
-    automated_report.report_class.constantize.new(*args).generate
-  end
-
-  def report_range
-    return nil unless needs_range?
-
-    n = 1
-    n = 3 if interval.quarterly?
-    n = 12 if interval.yearly?
-    range_end = n.months.since(range_start)
-    [range_start, range_end]
-  end
-
-  def step_width
-    return nil unless needs_step_width?
-    1
-  end
-
-  def needs_range?
-    REPORTS_THAT_NEED_RANGE.include?(automated_report.report_class)
-  end
-
-  def needs_step_width?
-    REPORTS_THAT_NEED_STEP_WIDTH.include?(automated_report.report_class)
-  end
-
-  REPORTS_THAT_NEED_RANGE = %w(
-    DailyDemandReport
-    FederationGrowthReport
-    FederatedSessionsReport
-    IdentityProviderDailyDemandReport
-    IdentityProviderSessionsReport
-    IdentityProviderDestinationServicesReport
-    ServiceProviderDailyDemandReport
-    ServiceProviderSessionsReport
-    ServiceProviderSourceIdentityProvidersReport
-  ).freeze
-
-  REPORTS_THAT_NEED_STEP_WIDTH = %w(
-    FederatedSessionsReport
-    IdentityProviderSessionsReport
-    ServiceProviderSessionsReport
-  ).freeze
 
   SUBSCRIBER_REPORTS = {
     'IdentityProviderSessionsReport' => IdentityProvider,
@@ -108,5 +54,4 @@ class AutomatedReportInstancesController < ApplicationController
   end
 
   private_constant :SUBSCRIBER_REPORTS
-  private_constant :REPORTS_THAT_NEED_RANGE, :REPORTS_THAT_NEED_STEP_WIDTH
 end
