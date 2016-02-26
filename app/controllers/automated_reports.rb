724c5d3206ba5b7a2ba2ab74a18c402ad2ddbc43
toomaj <toomaj_b@yahoo.com>
2016-02-26 14:20:44 +1000

    DRY automated report controllers

---
+++ app/controllers/automated_reports.rb
@@ -0,0 +1,43 @@
+class AutomatedReports < ApplicationController
+  private
+
+  SUBSCRIBER_REPORTS = {
+    'IdentityProviderSessionsReport' => IdentityProvider,
+    'IdentityProviderDailyDemandReport' => IdentityProvider,
+    'IdentityProviderDestinationServicesReport' => IdentityProvider,
+    'ServiceProviderSessionsReport' => ServiceProvider,
+    'ServiceProviderDailyDemandReport' => ServiceProvider,
+    'ServiceProviderSourceIdentityProvidersReport' => ServiceProvider
+  }.freeze
+
+  PUBLIC_REPORTS = %w(
+    DailyDemandReport
+    FederatedSessionsReport
+    FederationGrowthReport
+    IdentityProviderAttributesReport
+    ProvidedAttributeReport
+    RequestedAttributeReport
+    ServiceCompatibilityReport
+  ).freeze
+
+  def public_report?
+    PUBLIC_REPORTS.include?(report_class)
+  end
+
+  def subscriber_report?
+    SUBSCRIBER_REPORTS.keys.include?(report_class)
+  end
+
+  def subscriber_permissions
+    "objects:organization:#{entity.organization.identifier}:report"
+  end
+
+  def set_access_method
+    return public_action if public_report?
+    return check_access!(subscriber_permissions) if subscriber_report?
+
+    check_access! 'admin:report'
+  end
+
+  private_constant :SUBSCRIBER_REPORTS, :PUBLIC_REPORTS
+end
--- app/controllers/automated_reports_controller.rb
+++ app/controllers/automated_reports_controller.rb
@@ -1,4 +1,4 @@
-class AutomatedReportsController < ApplicationController
+class AutomatedReportsController < AutomatedReports
   before_action :set_subscriptions
   before_action :public_action, only: [:index, :destroy]
   before_action :set_access_method, only: :subscribe
@@ -92,39 +92,10 @@ class AutomatedReportsController < ApplicationController
     'ServiceProviderSourceIdentityProvidersReport' => ServiceProvider
   }.freeze
 
-  PUBLIC_REPORTS = %w(
-    DailyDemandReport
-    FederatedSessionsReport
-    FederationGrowthReport
-    IdentityProviderAttributesReport
-    ProvidedAttributeReport
-    RequestedAttributeReport
-    ServiceCompatibilityReport
-  ).freeze
-
-  def public_report?
-    PUBLIC_REPORTS.include?(report_class)
-  end
-
-  def subscriber_report?
-    SUBSCRIBER_REPORTS.keys.include?(report_class)
-  end
-
   def entity
     SUBSCRIBER_REPORTS[report_class]
       .find_by_identifying_attribute(target)
   end
 
-  def subscriber_permissions
-    "objects:organization:#{entity.organization.identifier}:report"
-  end
-
-  def set_access_method
-    return public_action if public_report?
-    return check_access!(subscriber_permissions) if subscriber_report?
-
-    check_access! 'admin:report'
-  end
-
-  private_constant :SUBSCRIBER_REPORTS, :PUBLIC_REPORTS
+  private_constant :SUBSCRIBER_REPORTS
 end
