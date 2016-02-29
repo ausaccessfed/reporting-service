953f392524fc9cbe1748dd4aede962cb49a7da03
toomaj <toomaj_b@yahoo.com>
2016-03-01 09:45:10 +1000

    refacto destory

--- app/controllers/automated_reports_controller.rb
+++ app/controllers/automated_reports_controller.rb
@@ -1,85 +1,37 @@
 class AutomatedReportsController < AutomatedReports
-  before_action :set_subscriptions
   before_action :public_action, only: [:index, :destroy]
   before_action :set_access_method, only: :subscribe
 
   def index
+    @subscriptions = subject_subscriptions
   end
 
   def subscribe
-    message = 'You have already subscribed to this report'
-    success_message = 'You have successfully subscribed to this report'
-
-    if !detect_subscription.blank?
-      flash[:notice] = message
-    elsif subscribe_to_report
-      flash[:notice] = success_message
-    end
-
     redirect_to :back
   end
 
   def destroy
-    subscription = @subscriptions
-                   .find_by(identifier: params[:identifier])
+    target_name = subscription.automated_report.target_name
+    flash[:target_name] = target_name if subscription.destroy
 
-    destroy_subscription subscription
     redirect_to automated_reports_path
   end
 
   private
 
-  def automated_report
-    report = AutomatedReport.find_by(interval: interval,
-                                     target: target,
-                                     report_class: report_class)
-
-    return report unless report.blank?
-
-    AutomatedReport.create(interval: interval,
-                           target: target,
-                           report_class: report_class)
-  end
-
-  def subscribe_to_report
-    return unless detect_subscription.blank?
-
+  def subject_subscriptions
     AutomatedReportSubscription
-      .create(subject: subject,
-              automated_report_id: automated_report.id,
-              identifier: SecureRandom.urlsafe_base64)
+      .where(subject: @subject)
+      .preload(:automated_report)
   end
 
-  def set_subscriptions
-    @subscriptions = AutomatedReportSubscription
-                     .where(subject_id: @subject.id)
-                     .preload(:automated_report)
+  def subscription
+    subject_subscriptions
+      .find_by(identifier: params[:identifier])
   end
 
-  def detect_subscription
-    @subscriptions.detect do |s|
-      s.automated_report.interval == interval &&
-        s.automated_report.report_class == report_class &&
-        s.automated_report.target == target
-    end
-  end
-
-  def destroy_subscription(subscription)
-    return unless subscription
-
-    message = subscription.automated_report.target_name
-    flash[:target_name] = message if subscription.destroy
-  end
-
-  def interval
-    params[:interval]
-  end
-
-  def target
-    params[:target]
-  end
-
-  def report_class
-    params[:report_class]
+  def automated_report_params
+    params.require(:automated_report)
+      .permit(:interval, :target, :report_class)
   end
 end
