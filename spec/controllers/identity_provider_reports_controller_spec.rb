29d99b3e6de42480406314d0dd84cb7a9b5c084e
toomaj <toomaj_b@yahoo.com>
2016-03-02 09:45:31 +1000

    move shared examples to support

--- spec/controllers/identity_provider_reports_controller_spec.rb
+++ spec/controllers/identity_provider_reports_controller_spec.rb
@@ -1,5 +1,4 @@
 require 'rails_helper'
-require 'controllers/subscriber_reports_controller'
 
 RSpec.describe IdentityProviderReportsController, type: :controller do
   let(:prefix) { 'identity' }
--- spec/controllers/service_provider_reports_controller_spec.rb
+++ spec/controllers/service_provider_reports_controller_spec.rb
@@ -1,5 +1,4 @@
 require 'rails_helper'
-require 'controllers/subscriber_reports_controller'
 
 RSpec.describe ServiceProviderReportsController, type: :controller do
   let(:prefix) { 'service' }
