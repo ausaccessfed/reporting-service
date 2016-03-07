259aaecc5bf40c642cf46e4af2f35d6c84b70cb1
toomaj <toomaj_b@yahoo.com>
2016-03-07 11:13:28 +1000

    add initial cretae_instance

--- app/jobs/create_automated_report_instances.rb
+++ app/jobs/create_automated_report_instances.rb
@@ -1,4 +1,26 @@
 class CreateAutomatedReportInstances
   def perform
+    create_instance
+  end
+
+  private
+
+  def automated_reports
+    AutomatedReport
+      .preload(:automated_report_subscriptions)
+      .select { |r| !r.automated_report_subscriptions.blank? }
+  end
+
+  def create_instance
+    automated_reports.each do |report|
+      AutomatedReportInstance
+        .create!(automated_report: report,
+                 identifier: SecureRandom.urlsafe_base64,
+                 range_start: current_time.beginning_of_month)
+    end
+  end
+
+  def current_time
+    Time.zone.now
   end
 end
--- spec/jobs/create_automated_report_instances_spec.rb
+++ spec/jobs/create_automated_report_instances_spec.rb
@@ -1,4 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe CreateAutomatedReportInstances do
+  around { |spec| Timecop.freeze { spec.run } }
+
+  let(:start_time) { Time.zone.parse('2016-01-04') }
+
+  let(:user) { create :subject }
+
+  INTERVALS = %w(monthly quarterly yearly).freeze
+
+  INTERVALS.each do |i|
+    let!("auto_report_#{i}".to_sym) do
+      create :automated_report,
+             interval: i,
+             report_class: 'DailyDemandReport'
+    end
+  end
+
+  before do
+    create :automated_report_subscription,
+           automated_report: auto_report_monthly,
+           subject: user
+  end
+
+  subject { CreateAutomatedReportInstances.new }
+
+  context 'perform' do
+    it 'includes automated reports with subscribers only' do
+      expect { subject.perform }
+        .to change { AutomatedReportInstance.count }
+        .from(0).to(1)
+    end
+  end
 end
