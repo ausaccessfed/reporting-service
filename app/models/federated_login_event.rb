fe12817e25f588f4e68a56efe589d1b33f05c205
toomaj <toomaj_b@yahoo.com>
2016-03-14 15:31:31 +1000

    Add #generate_record and remove unnecessary facotry

--- app/models/federated_login_event.rb
+++ app/models/federated_login_event.rb
@@ -1,3 +1,29 @@
 class FederatedLoginEvent < ActiveRecord::Base
   valhammer
+
+  def generate_record(ticket_string)
+    entries = fields(ticket_string)
+    FederatedLoginEvent.create! attributes(entries)
+  end
+
+  private
+
+  def fields(ticket_string)
+    attributes = ticket_string.split('#').map do |s|
+      k, v = s.split('=')
+      { k => v }
+    end
+
+    attributes.reduce(&:merge)
+  end
+
+  def attributes(entries)
+    {
+      relying_party: entries['RP'],
+      asserting_party: entries['AP'],
+      result: entries['RESULT'],
+      hashed_principal_name: SecureRandom.urlsafe_base64,
+      timestamp: Time.current
+    }
+  end
 end
--- spec/models/federated_login_event_spec.rb
+++ spec/models/federated_login_event_spec.rb
@@ -4,7 +4,18 @@ RSpec.describe FederatedLoginEvent, type: :model do
   around { |spec| Timecop.freeze { spec.run } }
 
   describe 'validations' do
-    subject { create :federated_login_event }
+    let(:ticket_string) do
+      'F-TICKS/AAF/1.0'\
+      '#TS=1457558279'\
+      '#RP=https://sp.example.edu/shibboleth'\
+      '#AP=https://idp.example.edu/idp/shibboleth'\
+      '#PN=72d2cce1bcda092e028ebf2a37a6001dcd6b444181fa2981100d12589b061942'\
+      '#RESULT=OK#'
+    end
+
+    subject { FederatedLoginEvent.new }
+
+    before { subject.generate_record(ticket_string) }
 
     it { is_expected.to validate_presence_of(:relying_party) }
     it { is_expected.to validate_presence_of(:asserting_party) }
