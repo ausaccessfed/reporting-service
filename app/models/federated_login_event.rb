9cb3bb6c98e91c38342b2781bdd26afa26a5d560
toomaj <toomaj_b@yahoo.com>
2016-03-15 11:41:48 +1000

    Add conditions to determine if timestamp string is all digits

--- app/models/federated_login_event.rb
+++ app/models/federated_login_event.rb
@@ -16,12 +16,13 @@ class FederatedLoginEvent < ActiveRecord::Base
   end
 
   def login_event_hash(entries)
-    {
-      relying_party: entries['RP'],
+    timestamp = nil
+    timestamp = Time.zone.at(entries['TS'].to_i) if entries['TS'] =~ /^\d+$/
+
+    { relying_party: entries['RP'],
       asserting_party: entries['AP'],
       result: entries['RESULT'],
       hashed_principal_name: entries['PN'],
-      timestamp: Time.zone.at(entries['TS'].to_i)
-    }
+      timestamp: timestamp }
   end
 end
