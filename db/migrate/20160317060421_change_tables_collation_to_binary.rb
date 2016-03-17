bec596559c08619dabaabd27ed3435cb72805bd2
toomaj <toomaj_b@yahoo.com>
2016-03-17 16:20:56 +1000

    Add migration to convert all tables collatios to UTF8 Fixes #135

---
+++ db/migrate/20160317060421_change_tables_collation_to_binary.rb
@@ -0,0 +1,18 @@
+class ChangeTablesCollationToBinary < ActiveRecord::Migration
+  TABLES = %w(activations api_subject_roles api_subjects
+              automated_report_instances automated_report_subscriptions
+              automated_reports discovery_service_events
+              federated_login_events identity_provider_saml_attributes
+              identity_providers incoming_f_ticks_events
+              organizations rapid_connect_services roles saml_attributes
+              service_provider_saml_attributes service_providers
+              subject_roles subjects).freeze
+
+  def change
+    TABLES.each do |table|
+      execute "ALTER TABLE #{table} COLLATE = utf8_bin"
+      execute "ALTER TABLE #{table} CONVERT TO CHARACTER"\
+              ' SET utf8 COLLATE utf8_bin'
+    end
+  end
+end
