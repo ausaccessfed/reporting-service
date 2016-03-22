3b1ab8bf1a774f0f22c24e71d2acef33343706dc
toomaj <toomaj_b@yahoo.com>
2016-03-22 11:34:15 +1000

    Change update! to update and create_instances to perform_create_instance

--- app/jobs/process_incoming_f_ticks_events.rb
+++ app/jobs/process_incoming_f_ticks_events.rb
@@ -2,16 +2,16 @@ class ProcessIncomingFTicksEvents
   def perform
     FederatedLoginEvent.transaction do
       incoming_events.find_each do |event|
-        create_instances(event)
+        perform_create_instance(event)
       end
     end
   end
 
   private
 
-  def create_instances(event)
+  def perform_create_instance(event)
     subject = FederatedLoginEvent.new
-    subject.create_instance(event) || event.update!(discarded: true)
+    subject.create_instance(event) || event.update(discarded: true)
   end
 
   def incoming_events
