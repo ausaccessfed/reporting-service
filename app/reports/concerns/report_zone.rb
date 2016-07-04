257fc22b6738adb7e0173875f43d7df2bc3abe77
toomaj <toomaj_b@yahoo.com>
2016-07-05 09:32:32 +1000

    Add report_zone concern, :convert_time_zone method & time_zone config

--- app/reports/concerns/report_zone.rb
+++ app/reports/concerns/report_zone.rb
@@ -1,3 +1,16 @@
 # frozen_string_literal: true
 module ReportZone
+  def convert_time_zone(time)
+    time.in_time_zone(zone)
+  end
+
+  private
+
+  def zone
+    configuration.time_zone
+  end
+
+  def configuration
+    Rails.application.config.reporting_service
+  end
 end
--- spec/reports/concerns/report_zone_spec.rb
+++ spec/reports/concerns/report_zone_spec.rb
@@ -2,4 +2,17 @@
 require 'rails_helper'
 
 RSpec.describe ReportZone do
+  prepend ReportZone
+
+  let(:time) { Faker::Number.number(4).to_i.days.ago.beginning_of_day }
+
+  describe '#in_time_zone' do
+    subject { convert_time_zone(time) }
+
+    context 'when user defined time_zone does not exist' do
+      it 'should set the report time_zone invoked from config' do
+        expect(subject.zone).to eq('AEST')
+      end
+    end
+  end
 end
