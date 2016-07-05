62432967a74ebdf43e4b194afa0ec94d4d9c36c6
toomaj <toomaj_b@yahoo.com>
2016-07-05 10:06:34 +1000

    Add create_time_instance_variables helper method

--- app/reports/concerns/report_time_zone.rb
+++ app/reports/concerns/report_time_zone.rb
@@ -1,5 +1,12 @@
 # frozen_string_literal: true
 module ReportTimeZone
+  def create_time_instance_variables(objects = {})
+    objects.each do |key, val|
+      value = convert_time_zone(val)
+      instance_variable_set("@#{key}", value)
+    end
+  end
+
   def convert_time_zone(time)
     time.in_time_zone(zone)
   end
--- spec/reports/concerns/report_time_zone_spec.rb
+++ spec/reports/concerns/report_time_zone_spec.rb
@@ -4,15 +4,28 @@ require 'rails_helper'
 RSpec.describe 'ReportTimeZone' do
   prepend ReportTimeZone
 
-  let(:time) { Faker::Number.number(4).to_i.days.ago.beginning_of_day }
+  let(:start) { Faker::Number.number(4).to_i.days.ago.beginning_of_day }
+  let(:finish) { Faker::Number.number(1).to_i.days.ago.beginning_of_day }
 
-  describe '#in_time_zone' do
-    subject { convert_time_zone(time) }
+  describe '#convert_time_zone' do
+    subject { convert_time_zone(start) }
 
     context 'when user defined time_zone does not exist' do
       it 'should set the report time_zone invoked from config' do
         expect(subject.zone).to eq('AEST')
       end
+    end
+  end
+
+  describe '#set_time_instance_variables' do
+    def run
+      create_time_instance_variables(start: start, finish: finish)
+    end
+
+    it 'should define instance variable based on key/value pairs' do
+      run
+      expect(@start).to eq(start)
+      expect(@finish).to eq(finish)
     end
   end
 end
