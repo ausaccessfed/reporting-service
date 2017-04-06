52ffdf52fc7cc7fdb18f250d7b7357d8b784e0e3
Shaun Mangelsdorf <s.mangelsdorf@gmail.com>
2017-04-06 10:57:11 +1000

    Create `public` directory before asset generation

--- config/initializers/static_errors.rb
+++ config/initializers/static_errors.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 require 'lipstick/errors/static_errors'
+require 'fileutils'
 
+FileUtils.mkdir_p Rails.root.join('public')
 Lipstick::StaticErrors.write_public_error_files
