73aa060b17bf577c878f152f40ec5358e36310fe
Bradley Beddoes <bradleybeddoes@gmail.com>
2018-05-24 13:07:42 +1000

    Version bump 1.0.1

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,7 @@ module ApplicationHelper
   include Lipstick::Helpers::NavHelper
   include Lipstick::Helpers::FormHelper
 
-  VERSION = '1.0.0'
+  VERSION = '1.0.1'
 
   def permitted?(action)
     @subject.try(:permits?, action)
