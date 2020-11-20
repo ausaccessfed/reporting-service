9e61b6d44b5bebc9faa689489c221b774aa0c721
bradleybeddoes <bradleybeddoes@gmail.com>
2020-11-20 10:24:13 +1000

    Bump to 1.4.0

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,7 @@ module ApplicationHelper
   include Lipstick::Helpers::NavHelper
   include Lipstick::Helpers::FormHelper
 
-  VERSION = '1.3.2'
+  VERSION = '1.4.0'
 
   # rubocop:disable Rails/HelperInstanceVariable
   def permitted?(action)
