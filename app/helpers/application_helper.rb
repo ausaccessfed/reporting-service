5499d32205998f51d8e3f3aba76f5d7bc638202e
Russell Ianniello <russell.ianniello@aaf.edu.au>
2021-03-03 11:05:03 +1000

    Increment version number

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,7 @@ module ApplicationHelper
   include Lipstick::Helpers::NavHelper
   include Lipstick::Helpers::FormHelper
 
-  VERSION = '1.4.0'
+  VERSION = '1.4.1'
 
   # rubocop:disable Rails/HelperInstanceVariable
   def permitted?(action)
