059c210fc99a0f9bec02118edef2a19dcd4f9ff8
Waldo Fouche <waldo.fouche@aaf.edu.au>
2021-09-15 11:42:28 +1000

    Update version to `1.4.2`

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,7 @@ module ApplicationHelper
   include Lipstick::Helpers::NavHelper
   include Lipstick::Helpers::FormHelper
 
-  VERSION = '1.4.1'
+  VERSION = '1.4.2'
 
   # rubocop:disable Rails/HelperInstanceVariable
   def permitted?(action)
