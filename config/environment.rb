16d7e37a7f174153a6bf850e11e1854f222670a6
Waldo Fouche <waldo.fouche@aaf.edu.au>
2022-03-01 10:24:14 +0800

    Update using `Rails app:update`
    Manually checked the changes and applied relevant ones

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,5 @@
-# frozen_string_literal: true
+# Load the Rails application.
+require_relative 'application'
 
-require File.expand_path('application', __dir__)
-
+# Initialize the Rails application.
 Rails.application.initialize!
