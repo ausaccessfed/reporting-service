a55c10d6bfbc134ceb525865cae3e3052c63724b
Waldo Fouche <waldo.fouche@aaf.edu.au>
2022-03-02 10:01:51 +0800

    Restore some previous configurations for prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,12 +23,16 @@ Rails.application.configure do
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.public_file_server.headers = {
+    'Cache-Control' => "public, max-age=#{1.day.to_i}"
+  }
 
   # Compress CSS using a preprocessor.
-  # config.assets.css_compressor = :sass
+  config.assets.js_compressor = :uglifier
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
+  config.assets.digest = true
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
