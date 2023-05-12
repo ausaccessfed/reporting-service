a5212b45fbf38a6661062e4a1def9f416c35ab03
phyzical <5182053+phyzical@users.noreply.github.com>
2023-05-12 11:02:51 +0800

    try an int port?

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -8,7 +8,7 @@ preload_app true
 pid File.join(ROOT, 'tmp', 'pids', 'unicorn.pid')
 stdout_path ENV.fetch('STDOUT', '/var/log/aaf/reporting/puma/stdout.log')
 stderr_path ENV.fetch('STDERR', '/var/log/aaf/reporting/puma/stderr.log')
-listen ENV.fetch('PORT', 8080)
+listen ENV.fetch('PORT', 8080).to_i
 before_fork do |server, _worker|
   old_pid = File.join(ROOT, 'tmp', 'pids', 'unicorn.pid.oldbin')
   if File.exist?(old_pid) && server.pid != old_pid
