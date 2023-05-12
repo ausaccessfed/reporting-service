3d225b289b38315f93e73d46a551044cd370e837
phyzical <5182053+phyzical@users.noreply.github.com>
2023-05-12 14:25:04 +0800

    fix config reference

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -4,7 +4,7 @@ redis_namespace = "reporting-service:#{Rails.env}:session"
 session_cookie_secure = Rails.env.production?
 
 session_store_opts = {
-  redis_server: "#{config.reporting_service.redis[:url]}/#{redis_namespace}",
+  redis_server: "#{Rails.application.config.reporting_service.redis[:url]}/#{redis_namespace}",
   expire_after: 3600,
   secure: session_cookie_secure,
   key: '_reporting-service-session'
