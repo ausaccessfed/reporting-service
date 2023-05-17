da5321757a6719488fc59de0af2ad0f18d588bbc
phyzical <5182053+phyzical@users.noreply.github.com>
2023-05-17 13:53:43 +0800

    change all back

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,7 +13,7 @@ Rails.application.configure do
   config.eager_load = true
 
   # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local       = true
+  config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
@@ -49,7 +49,7 @@ Rails.application.configure do
   config.log_level = :debug
 
   # Prepend all log lines with the following tags.
-  # config.log_tags = [:request_id]
+  config.log_tags = [:request_id]
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
@@ -66,18 +66,17 @@ Rails.application.configure do
   config.active_support.deprecation = :notify
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
-  # config.log_formatter = ::Logger::Formatter.new
+  config.log_formatter = ::Logger::Formatter.new
 
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
   if ENV['RAILS_LOG_TO_STDOUT'].present?
-    config.logger = ActiveSupport::Logger.new(ENV.fetch('STDOUT', $stdout))
-    # logger           = ActiveSupport::Logger.new(ENV.fetch('STDOUT', $stdout))
-    # logger.formatter = config.log_formatter
-    # config.logger    = ActiveSupport::TaggedLogging.new(logger)
-    # config.lograge.enabled = true
-    # config.lograge.ignore_actions = ['HealthController#show', 'WelcomeController#index']
+    logger           = ActiveSupport::Logger.new(ENV.fetch('STDOUT', $stdout))
+    logger.formatter = config.log_formatter
+    config.logger    = ActiveSupport::TaggedLogging.new(logger)
+    config.lograge.enabled = true
+    config.lograge.ignore_actions = ['HealthController#show', 'WelcomeController#index']
   end
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
