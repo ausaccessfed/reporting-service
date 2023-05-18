db142a4923865c107787c40b71a94d268f6bfab7
phyzical <5182053+phyzical@users.noreply.github.com>
2023-05-18 12:58:29 +0800

    try different sessions tore

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,12 +1,15 @@
 # frozen_string_literal: true
 
-redis_namespace = "#{Rails.application.config.reporting_service.redis[:namespace]}:#{Rails.env}:session"
+# redis_namespace = "#{Rails.application.config.reporting_service.redis[:namespace]}:#{Rails.env}:session"
+# session_cookie_secure = Rails.env.production?
 
-session_store_opts = {
-  redis_server: "#{Rails.application.config.reporting_service.redis[:url]}/#{redis_namespace}",
-  expire_after: 3600,
-  secure: false,
-  key: "_#{Rails.application.config.reporting_service.redis[:namespace]}-session"
-}
+# session_store_opts = {
+#   redis_server: "#{Rails.application.config.reporting_service.redis[:url]}/#{redis_namespace}",
+#   expire_after: 3600,
+#   secure: session_cookie_secure,
+#   key: "_#{Rails.application.config.reporting_service.redis[:namespace]}-session"
+# }
 
-Rails.application.config.session_store(:redis_store, session_store_opts)
+# Rails.application.config.session_store(:redis_store, session_store_opts)
+Rails.application.config.session_store :active_record_store,
+                                       key: "_#{Rails.application.config.reporting_service.redis[:namespace]}_session"
