4ee4af26d1a6e16810f4858a47115dab10ba9525
phyzical <5182053+phyzical@users.noreply.github.com>
2023-05-18 09:34:09 +0800

    disable lograge again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,8 +75,8 @@ Rails.application.configure do
     logger           = ActiveSupport::Logger.new(ENV.fetch('STDOUT', $stdout))
     logger.formatter = config.log_formatter
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
-    config.lograge.enabled = true
-    config.lograge.ignore_actions = ['HealthController#show', 'WelcomeController#index']
+    # config.lograge.enabled = true
+    # config.lograge.ignore_actions = ['HealthController#show', 'WelcomeController#index']
   end
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
