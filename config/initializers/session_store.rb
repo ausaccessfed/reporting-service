719f0cd9d0524107e0b6b1f748f24ebdf9398376
phyzical <5182053+phyzical@users.noreply.github.com>
2023-05-29 09:22:37 +0800

    try disabling ssl for session

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
-# redis_namespace = "#{Rails.application.config.reporting_service.redis[:namespace]}:#{Rails.env}:session"
-# session_cookie_secure = Rails.env.production?
+redis_namespace = "#{Rails.application.config.reporting_service.redis[:namespace]}:#{Rails.env}:session"
+session_cookie_secure = false
 
-# session_store_opts = {
-#   redis_server: "#{Rails.application.config.reporting_service.redis[:url]}/#{redis_namespace}",
-#   expire_after: 3600,
-#   secure: session_cookie_secure,
-#   key: "_#{Rails.application.config.reporting_service.redis[:namespace]}-session"
-# }
+session_store_opts = {
+  redis_server: "#{Rails.application.config.reporting_service.redis[:url]}/#{redis_namespace}",
+  expire_after: 3600,
+  secure: session_cookie_secure,
+  key: "_#{Rails.application.config.reporting_service.redis[:namespace]}-session"
+}
 
-# Rails.application.config.session_store(:redis_store, session_store_opts)
+Rails.application.config.session_store(:redis_store, session_store_opts)
