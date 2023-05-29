350b087841f487444f07fe1c1631a755d5ae57a8
phyzical <5182053+phyzical@users.noreply.github.com>
2023-05-29 10:01:36 +0800

    try providing servers instead?

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 redis_namespace = "#{Rails.application.config.reporting_service.redis[:namespace]}:#{Rails.env}:session"
-session_cookie_secure = Rails.env.production?
+session_cookie_secure = false
 
 session_store_opts = {
-  redis_server: "#{Rails.application.config.reporting_service.redis[:url]}/#{redis_namespace}?ssl_cert_reqs=CERT_NONE",
+  servers: ["#{Rails.application.config.reporting_service.redis[:url]}/#{redis_namespace}?ssl_cert_reqs=CERT_NONE"],
   expire_after: 3600,
   secure: session_cookie_secure,
   key: "_#{Rails.application.config.reporting_service.redis[:namespace]}-session"
