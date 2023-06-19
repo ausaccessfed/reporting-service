cb83dc52646c5e1be7af666511119c3ebf2d2b87
Wibowo Sutanto <bo-aaf@users.noreply.github.com>
2023-06-19 10:17:10 +1000

    Have Sentry ignore Puma::HttpParserError
    Puma raises this error when a client makes a malformed HTTP request.
    It responds appropriately with 400 Bad Request.
    We don't need to hear about that!

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -9,5 +9,10 @@ Sentry.init do |config|
   config.logger = Logger.new(ENV.fetch('STDOUT', $stdout))
   config.logger.level = Logger::WARN
   config.include_local_variables = true
+
+  # Puma raises this error when a client makes a malformed HTTP request. It responds appropriately with 400 Bad Request.
+  # We don't need to hear about that!
+  # See https://github.com/getsentry/sentry-ruby/pull/2026#issuecomment-1525031744.
+  config.excluded_exceptions << 'Puma::HttpParserError'
 end
 # :nocov:
