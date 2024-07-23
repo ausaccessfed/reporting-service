646b4874e46b78596846e47d1fbe98adfdbb2496
phyzical <5182053+phyzical@users.noreply.github.com>
2024-07-23 13:54:09 +0800

    remove fake redis

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,8 +6,6 @@ require 'support/capybara_setup'
 
 SimpleCov.formatter = SimpleCov::Formatter::Console
 RSpec.configure do |config|
-  config.before { Redis::Connection::Memory.reset_all_databases }
-
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
