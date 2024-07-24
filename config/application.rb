fa165fcc2f7d852997110e4dcfa9641898ca9f91
phyzical <5182053+phyzical@users.noreply.github.com>
2024-07-24 13:54:37 +0800

    dotenv only dev and test

--- config/application.rb
+++ config/application.rb
@@ -3,6 +3,7 @@
 require File.expand_path('boot', __dir__)
 
 require 'rails'
+require 'dotenv/load' unless Rails.env.production?
 require 'active_model/railtie'
 require 'active_record/railtie'
 require 'action_controller/railtie'
