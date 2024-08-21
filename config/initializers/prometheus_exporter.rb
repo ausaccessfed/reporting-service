82b96d162d1f812a12c610cc9655efe9971b4652
phyzical <5182053+phyzical@users.noreply.github.com>
2024-08-21 14:54:19 +0800

    linting

--- config/initializers/prometheus_exporter.rb
+++ config/initializers/prometheus_exporter.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-unless Rails.env.test? && ENV.fetch('PROMETHEUS_METRICS_SERVICE_HOST', '') != '' # rubocop:disable Rails/EnvironmentVariableAccess
+if !Rails.env.test? && ENV.fetch('PROMETHEUS_METRICS_SERVICE_HOST', '') != ''
   require 'prometheus_exporter/server'
   require 'prometheus_exporter/client'
   require 'prometheus_exporter/instrumentation'
@@ -8,13 +8,13 @@ unless Rails.env.test? && ENV.fetch('PROMETHEUS_METRICS_SERVICE_HOST', '') != ''
 
   PrometheusExporter::Client.default =
     PrometheusExporter::Client.new(
-      host: ENV.fetch('PROMETHEUS_METRICS_SERVICE_HOST', 'reporting-service-metrics.development.svc.cluster.local'), # rubocop:disable Rails/EnvironmentVariableAccess
-      port: ENV.fetch('PROMETHEUS_METRICS_SERVICE_PORT', '9493').to_i, # rubocop:disable Rails/EnvironmentVariableAccess
+      host: ENV.fetch('PROMETHEUS_METRICS_SERVICE_HOST', 'reporting-service-metrics.development.svc.cluster.local'),
+      port: ENV.fetch('PROMETHEUS_METRICS_SERVICE_PORT', '9493').to_i,
       custom_labels: {
         hostname: PrometheusExporter.hostname,
         env: Rails.env,
         app: 'reporting-service',
-        pod: ENV.fetch('HOSTNAME', 'unknown') # rubocop:disable Rails/EnvironmentVariableAccess
+        pod: ENV.fetch('HOSTNAME', 'unknown')
       }
     )
 
@@ -26,7 +26,7 @@ unless Rails.env.test? && ENV.fetch('PROMETHEUS_METRICS_SERVICE_HOST', '') != ''
   end
 
   # this reports basic process stats like RSS and GC info
-  PrometheusExporter::Instrumentation::Process.start(type: ENV.fetch('PROMETHEUS_TYPE', 'server')) # rubocop:disable Rails/EnvironmentVariableAccess
+  PrometheusExporter::Instrumentation::Process.start(type: ENV.fetch('PROMETHEUS_TYPE', 'server'))
   # This reports stats per request like HTTP status and timings
   Rails.application.middleware.unshift PrometheusExporter::Middleware
 end
